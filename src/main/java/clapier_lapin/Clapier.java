ecc73dbf149f4bcd7df8285155a8eec737eb7f78
erdemcicek <erdem25123@gmail>
2022-10-25 00:20:23 +0200

    clapier lapin with test

---
+++ src/main/java/clapier_lapin/Clapier.java
@@ -0,0 +1,166 @@
+package clapier_lapin;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+
+public class Clapier {
+	
+	public static class Lapin{
+		
+		public Lapin() {
+			enfants = new HashSet<>();
+		}
+		 
+		//! I used this part just for demonstration purposes
+//		public Lapin(String name) {
+//			this();
+//			this.name = name;
+//		}
+//		
+//		String name;
+//		public String toString() {
+//			return name;
+//		}
+
+		Lapin pere;
+		
+		public void addChild(Lapin enfant) {
+			this.enfants.add(enfant);
+			// Cette attribution en fait casse le principe "Single Responsibility"
+			enfant.pere = this;
+		}
+		
+		
+		/*
+		Je n'ai pas voulu changer votre code 
+		Parce qu'on avait utilisé les attributions 
+			telles que "enfant = enfant.pere" pour faire itération dans la méthode "estAncestre"
+		
+		//! Dans ce setter, on fait une attribution et on appelle une autre méthode
+		//! Ceci est, donc, une implementation plus pertinent à cette règle (SRP) à mon avis
+
+		public void setPere(Lapin pere) {
+			this.pere = pere;
+			addChild();
+		}
+		
+		public void addChild() {
+			this.enfants.add(this);
+		}
+		
+		*/
+		
+		Set<Lapin> enfants;
+		
+		public Set<Lapin> getAllLevelChildren(){
+			Set<Lapin> set_a_retourner = new HashSet<>();
+			
+			Queue<Lapin> q = new LinkedList<>();
+			q.addAll(enfants);
+			
+			while(!q.isEmpty()) {
+				Lapin front = q.poll();
+				set_a_retourner.add(front);
+				
+				if(!front.enfants.isEmpty()) {
+					for(Lapin lapin : front.enfants) {
+						q.add(lapin);
+					}
+				}
+			}
+			
+			return set_a_retourner;
+		}
+		
+	}
+	
+	public static boolean estAncestre(Lapin enfant, Lapin ancestre) {
+		if(enfant == null || ancestre == null) return false;
+		
+		// Solution with Iterative Approach
+		while(enfant.pere != null) {
+			if(enfant.pere == ancestre) return true;
+			enfant = enfant.pere;
+		}
+		
+		return false;
+		
+		/*  Solution with Recursive Approach
+		if(enfant.pere == ancestre) return true;
+		
+		return estAncestre(enfant.pere, ancestre);
+		*/
+	}
+	
+	public static boolean estCyclique(Lapin l1, Lapin l2) {
+		
+		if(l1 == null || l2 == null) return false;
+		
+		Lapin oneStepIterator = l1;
+		Lapin twoStepIterator = l1;
+		// Puisqu'on contrôle l'existence d'un cycle juste en tenant compte de "l1"
+		// Il faut aussi contrôler si "l2" est aussi sur ce cercle cyclique
+		boolean is_l2_on_the_cyclic_path = false;
+				
+		// S'il y a un cercle, les deux iterators vont finalement se croiser 
+		// Sinon on va quitter le boucle while, parce que l'un des valeurs ci-dessous serait égale à null
+		while(twoStepIterator != null &&  twoStepIterator.pere != null) {
+			// Il faut checker si "l2" est sur le cercle cyclique
+			if(oneStepIterator == l2 || twoStepIterator == l2 ) is_l2_on_the_cyclic_path = true;
+			
+			oneStepIterator = oneStepIterator.pere;
+			twoStepIterator = twoStepIterator.pere.pere;
+			
+			if(oneStepIterator == twoStepIterator) {
+				// Ce contrôle est très important
+				// Si on combine ces deux "if" comme if(oneStepIterator == twoStepIterator && is_l2_on_the_cyclic_path) 
+				// Cela causerait un boucle infini
+				if(is_l2_on_the_cyclic_path) return true;
+				// Si les deux iterator se croise mais "l2" n'est pas sur le chemin
+				// On retourne simplement false
+				return false;
+			}
+		}
+		
+		return false;
+	}
+	
+	public static List<Lapin> estAncestre(List<Lapin> enfants, Lapin ancestre){
+		return ancestre.getAllLevelChildren().stream().filter(element -> enfants.contains(element)).collect(Collectors.toList());
+	}
+	
+	/*
+	  
+	 					 		Lap1 								Lap9 ( Ce Lapin n'a aucune relation )
+	 			Lap2			Lap3		Lap4
+	 						Lap5    Lap6        Lap7
+	 												Lap8
+	 												
+	 	Lap1 ==>	 père de 1,3,4
+	 	Lap2 ==>	 aucun enfant
+	 	Lap3 ==>	 père de 5,6
+	 	Lap4 ==>	 père de 7
+	 	Lap5 ==>	 aucun enfant
+	 	Lap6 ==>	 père de 7
+	 	Lap7 ==>	 père de 8
+	 	Lap8 ==>	 aucun enfant
+	 	Lap9 ==> 	 n'a aucune relation
+	 */
+	/*
+	
+	Si j'ai bien compris lors de l'entretien ;
+	Si je passe une Liste de {Lap5, Lap6, Lap7, Lap2, Lap8, Lap9} avec Lap1 comme ancestre
+	Cela doit retourner {Lap5, Lap6, Lap7, Lap2, Lap8}
+	Si je passe une Liste de {Lap5, Lap7, Lap2, Lap9} avec Lap3 comme ancestre
+	Cela doit retourner {Lap5}, parce qu'il n'y a pas Lap6, l'autre enfant de Lap3, dans la liste
+	
+	 */
+	
+	
+}
+
---
+++ src/test/java/clapier_lapin/ClapierTest.java
@@ -0,0 +1,259 @@
+package clapier_lapin;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+
+public class ClapierTest {
+	
+	@Test
+	public void estAncestre_whenThereIsDirectParentChildRelationship_returnsTrue() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin parent = new Clapier.Lapin();
+		enfant.pere = parent;
+		assertTrue(Clapier.estAncestre(enfant, parent));
+	}
+	
+	@Test
+	public void estAncestre_whenAddChildMethodIsUsed_receiveParentOfChildIsAssigned() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin parent = new Clapier.Lapin();
+		parent.addChild(enfant);
+		assertTrue(Clapier.estAncestre(enfant, parent));
+	}
+	
+	@Test
+	public void estAncestre_whenThereIsNoParentChildRelationship_returnsFalse() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin other = new Clapier.Lapin();
+		assertFalse(Clapier.estAncestre(enfant, other));
+	}
+	
+	@Test
+	public void estAncestre_whenOneOfTheLapinsIsNull_returnsFalse() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin nullLapin = null;
+		assertFalse(Clapier.estAncestre(enfant, nullLapin));
+		assertFalse(Clapier.estAncestre(nullLapin, enfant));
+	}
+	
+	
+	@Test
+	public void estAncestre_whenThereIsTwoDegreeParentChildRelationship_returnsTrue() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin parent = new Clapier.Lapin();
+		enfant.pere = parent;
+		Clapier.Lapin grandParent = new Clapier.Lapin();
+		parent.pere = grandParent;
+		assertTrue(Clapier.estAncestre(enfant, grandParent));
+	}
+	
+	@Test
+	public void estAncestre_whenThereIsMoreThanTwoDegreeParentChildRelationship_returnsTrue() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin parent = new Clapier.Lapin();
+		enfant.pere = parent;
+		
+		Clapier.Lapin grandParent = new Clapier.Lapin();
+		parent.pere = grandParent;
+		
+		Clapier.Lapin grandGrandParent = new Clapier.Lapin();
+		grandParent.pere = grandGrandParent;
+		
+		assertTrue(Clapier.estAncestre(enfant, grandGrandParent));
+	}
+	
+	@Test
+	public void estCyclique_whenThereIsNoCyclicRelationship_returnsFalse() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin parent = new Clapier.Lapin();
+		enfant.pere = parent;
+		
+		Clapier.Lapin grandParent = new Clapier.Lapin();
+		parent.pere = grandParent;
+		
+		Clapier.Lapin grandGrandParent = new Clapier.Lapin();
+		grandParent.pere = grandGrandParent;
+		
+		assertFalse(Clapier.estCyclique(enfant, grandGrandParent));
+	}
+	
+	@Test
+	public void estCyclique_whenThereIsCyclicRelationship_returnsTrue() {
+		
+		// Non-Cyclic Part
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin parent = new Clapier.Lapin();
+		enfant.pere = parent;
+		
+		Clapier.Lapin grandParent = new Clapier.Lapin();
+		parent.pere = grandParent;
+		
+		Clapier.Lapin grandGrandParent = new Clapier.Lapin();
+		grandParent.pere = grandGrandParent;
+		
+		// With this assignment, it becomes Cyclic
+		grandGrandParent.pere = enfant;
+		
+		assertTrue(Clapier.estCyclique(enfant, grandGrandParent));
+	}
+	
+	@Test
+	public void estCyclique_whenThereIsCycleButNotBetweenTheParametersBeingPassed_returnsFalse() {
+		// Non-Cyclic Part
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin parent = new Clapier.Lapin();
+		enfant.pere = parent;
+		
+		Clapier.Lapin grandParent = new Clapier.Lapin();
+		parent.pere = grandParent;
+		
+		Clapier.Lapin grandGrandParent = new Clapier.Lapin();
+		grandParent.pere = grandGrandParent;
+		
+		// With this assignment, it becomes Cyclic
+		grandGrandParent.pere = enfant;
+		
+		// Here a brand new Lapin that does not involve in the cycle
+		Clapier.Lapin other = new Clapier.Lapin();
+
+		assertFalse(Clapier.estCyclique(other, grandGrandParent));
+		assertFalse(Clapier.estCyclique(grandGrandParent, other));
+
+	}
+	
+	@Test
+	public void estCyclique_whenOneOfTheLapinsIsNull_returnsFalse() {
+		Clapier.Lapin enfant = new Clapier.Lapin();
+		Clapier.Lapin nullLapin = null;
+		assertFalse(Clapier.estCyclique(enfant, nullLapin));
+		assertFalse(Clapier.estCyclique(nullLapin, enfant));
+	}
+	
+	// One_Exactly_Match
+	@Test
+	public void estAncestre_whenThereIsOneDegreeRelationshipAndFamilyTreeAndParameterListAreExactlyMatched_returnsSetTheSameAsSetOfChildren() {
+		
+		Clapier.Lapin lap1 = new Clapier.Lapin();
+		Clapier.Lapin lap2 = new Clapier.Lapin();
+		Clapier.Lapin lap3 = new Clapier.Lapin();
+		// Lap1 est le pere de Lap2 et Lap3
+		lap1.addChild(lap2);
+		lap1.addChild(lap3);
+		
+		assertTrue(lap1.enfants.containsAll(Clapier.estAncestre(List.of(lap2, lap3), lap1)));
+
+	}
+	
+	// One_Family_Tree_Has_More
+	@Test
+	public void estAncestre_whenThereIsOneDegreeRelationshipAndFamilyTreeHasMoreElementThanParameterList_returnsSetAsSubsetOfSetOfChildren() {
+		
+		Clapier.Lapin lap1 = new Clapier.Lapin();
+		Clapier.Lapin lap2 = new Clapier.Lapin();
+		Clapier.Lapin lap3 = new Clapier.Lapin();
+		// Lap1 est le pere de Lap2 et Lap3
+		lap1.addChild(lap2);
+		lap1.addChild(lap3);
+		
+		//Clapier.Lapin lap4 = new Clapier.Lapin();
+
+		assertTrue(lap1.enfants.containsAll(Clapier.estAncestre(List.of(lap2), lap1)));
+	}
+	
+	// One_List_Extra
+	@Test
+	public void estAncestre_whenThereIsOneDegreeRelationshipAndFamilyTreeHasMoreElementThanParameterListAndListHasExtraElement_returnsSetAsSubsetOfSetOfChildren() {
+		
+		Clapier.Lapin lap1 = new Clapier.Lapin();
+		Clapier.Lapin lap2 = new Clapier.Lapin();
+		Clapier.Lapin lap3 = new Clapier.Lapin();
+		// Lap1 est le pere de Lap2 et Lap3
+		lap1.addChild(lap2);
+		lap1.addChild(lap3);
+		
+		Clapier.Lapin lap4 = new Clapier.Lapin();
+		Clapier.Lapin lap5 = new Clapier.Lapin();
+
+
+		assertTrue(lap1.enfants.containsAll(Clapier.estAncestre(List.of(lap2, lap4, lap5), lap1)));
+	}
+	
+	// Two_Exactly_Match
+	@Test
+	public void estAncestre_whenThereIsTwoDegreeRelationshipAndFamilyTreeAndParameterListAreExactlyMatched_returnsSetTheSameAsSetOfChildren() {
+		
+		Clapier.Lapin lap1 = new Clapier.Lapin();
+		Clapier.Lapin lap2 = new Clapier.Lapin();
+		Clapier.Lapin lap3 = new Clapier.Lapin();
+		// Lap1 est le pere de Lap2 et Lap3
+		lap1.addChild(lap2);
+		lap1.addChild(lap3);
+		
+		Clapier.Lapin lap4 = new Clapier.Lapin();
+		Clapier.Lapin lap5 = new Clapier.Lapin();
+		// Lap 2 est le pere de Lap4
+		lap2.addChild(lap4);
+		// Lap 3 est le pere de Lap5
+		lap3.addChild(lap5);
+		
+		Set<Clapier.Lapin> setAttendu = lap1.getAllLevelChildren();
+
+		assertTrue(setAttendu.containsAll(Clapier.estAncestre(List.of(lap2, lap3, lap4, lap5), lap1)));
+
+	}
+	
+	// One_Family_Tree_Has_More
+	@Test
+	public void estAncestre_whenThereIsTwoDegreeRelationshipAndFamilyTreeHasMoreElementThanParameterList_returnsSetAsSubsetOfSetOfChildren() {
+		Clapier.Lapin lap1 = new Clapier.Lapin();
+		Clapier.Lapin lap2 = new Clapier.Lapin();
+		Clapier.Lapin lap3 = new Clapier.Lapin();
+		// Lap1 est le pere de Lap2 et Lap3
+		lap1.addChild(lap2);
+		lap1.addChild(lap3);
+		
+		Clapier.Lapin lap4 = new Clapier.Lapin();
+		Clapier.Lapin lap5 = new Clapier.Lapin();
+		// Lap 2 est le pere de Lap4
+		lap2.addChild(lap4);
+		// Lap 3 est le pere de Lap5
+		lap3.addChild(lap5);
+		
+		Set<Clapier.Lapin> setAttendu = lap1.getAllLevelChildren();
+
+		assertTrue(setAttendu.containsAll(Clapier.estAncestre(List.of(lap2, lap5), lap1)));
+	}
+	
+	// Two_List_Extra
+	@Test
+	public void estAncestre_whenThereIsTwoDegreeRelationshipAndFamilyTreeHasMoreElementThanParameterListAndListHasExtraElement_returnsSetAsSubsetOfSetOfChildren() {
+		
+		Clapier.Lapin lap1 = new Clapier.Lapin();
+		Clapier.Lapin lap2 = new Clapier.Lapin();
+		Clapier.Lapin lap3 = new Clapier.Lapin();
+		// Lap1 est le pere de Lap2 et Lap3
+		lap1.addChild(lap2);
+		lap1.addChild(lap3);
+		
+		Clapier.Lapin lap4 = new Clapier.Lapin();
+		Clapier.Lapin lap5 = new Clapier.Lapin();
+		// Lap 2 est le pere de Lap4
+		lap2.addChild(lap4);
+		// Lap 3 est le pere de Lap5
+		lap3.addChild(lap5);
+		
+		Clapier.Lapin lap6 = new Clapier.Lapin();
+		Clapier.Lapin lap7 = new Clapier.Lapin();
+
+		Set<Clapier.Lapin> setAttendu = lap1.getAllLevelChildren();
+		
+		assertTrue(setAttendu.containsAll(Clapier.estAncestre(List.of(lap2, lap3, lap4, lap6, lap7), lap1)));
+
+	}
+
+}
